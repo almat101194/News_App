1f7fa547ee54b8fa141530b1f4857dffcb68fca6
MySterioHero <almat101194@gmail.com>
2020-10-06 16:33:42 +0600

    beginnig

---
+++ app/src/main/java/kz/almat/newsapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package kz.almat.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //Start to Write
+    }
+}
